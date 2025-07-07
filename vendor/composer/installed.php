c963c66e731f87f875525ead55f96369099c9ce8
Muhammad Rafli Adyatma <rafliadyatma17@gmail.com>
2025-07-07 21:35:52 +0700

    sail config

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '12c57ea5d687f8f4cb896f71dd4468c3bc7d52c8',
+        'reference' => '344b81fcedf8603f10d007a2a63f3576a1a53c12',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '12c57ea5d687f8f4cb896f71dd4468c3bc7d52c8',
+            'reference' => '344b81fcedf8603f10d007a2a63f3576a1a53c12',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
